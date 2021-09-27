c61d1a59e1cad34f193584316a0f09ef62b24aa3
hai anh <anhndhse151389@fpt.edu.vn>
2021-09-28 04:31:24 +0700

    commit

--- src/main/java/com/haianh/springsecurity/appuser/MyUserDetails.java
+++ src/main/java/com/haianh/springsecurity/appuser/MyUserDetails.java
@@ -1,2 +1,65 @@
-package com.haianh.springsecurity.appuser;public class MyUserDetails {
+package com.haianh.springsecurity.appuser;
+
+import com.haianh.springsecurity.student.Student;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyUserDetails implements UserDetails {
+
+    private String userName;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public MyUserDetails(Student student) {
+        this.userName = student.getName();
+        this.password = student.getPassword();
+        this.authorities = Arrays.stream(student.getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    public MyUserDetails() {
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
